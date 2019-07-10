267e206d59f7e2995951aa28ac974205c509956a
YeThor <kysloves@naver.com>
2019-07-10 22:36:54 +0900

    ✨ save token value in chrome.storage

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
-import { fromEvent } from "rxjs";
+import { fromEvent, Observable } from "rxjs";
 import { take } from "rxjs/operators";
 
-const initializeApp = (): void => {
-  const tokenInput = document.querySelector("#token-input");
-  const onTokenSaveBtn$ = fromEvent(
-    document.querySelector("#token-btn") as HTMLButtonElement,
-    "click"
-  );
+const initializeApp = (): App => new App();
 
-  onTokenSaveBtn$.subscribe(() => {
-    console.log("clicked");
-  });
-};
+class App {
+  private _onTokenBtnClick$: Observable<Event>;
+
+  constructor() {
+    this._onTokenBtnClick$ = fromEvent(
+      document.querySelector("#token-btn") as HTMLButtonElement,
+      "click"
+    );
+
+    this._init();
+  }
+
+  private _init(): void {
+    this._watchObservables();
+  }
+
+  private _watchObservables(): void {
+    this._onTokenBtnClick$.subscribe(this._storeToken);
+  }
+
+  private _storeToken(): void {
+    const token = (document.querySelector("#token-input") as HTMLInputElement)
+      .value;
+
+    chrome.storage.sync.set({ token }, () => {
+      console.log(`just saved ${token}`);
+    });
+  }
+}
 
 window.addEventListener("DOMContentLoaded", initializeApp);
