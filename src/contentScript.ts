d4edf712636af705cc758f96667d0fde47be8e5b
YeThor <kysloves@naver.com>
2019-07-12 23:18:18 +0900

    ✨ use a token saved in chrome storage when using github API

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,14 +1,20 @@
-console.log("test");
+import { getToken } from "./util";
 
-const PERSONAL_TOKEN = "check your memo";
+console.log("content-script");
 
-fetch("https://api.github.com/repos/YeThor/Pin-Github/issues", {
-  method: "POST",
-  headers: new Headers({
-    Authorization: `token ${PERSONAL_TOKEN}`
-  }),
-  body: JSON.stringify({
-    title: "Test Title",
-    body: "Test Body"
-  })
-}).then(res => console.log(res));
+getToken().then(
+  (token: string): void => {
+    fetch("https://api.github.com/repos/YeThor/Pin-Github/issues", {
+      method: "POST",
+      headers: new Headers({
+        Authorization: `token ${token}`
+      }),
+      body: JSON.stringify({
+        title: "Test Title",
+        body: "Test Body"
+      })
+    }).then(res => console.log(res));
+  }
+);
+
+console.log("abasc");
