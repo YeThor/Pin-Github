a3d10f416ecffcde826c87eed01b872efec667b9
YeThor <kysloves@naver.com>
2019-07-13 20:08:22 +0900

    :sparkles: detect a NEW ISSUE button

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,19 +2,32 @@ import getToken from "./util/getToken";
 
 console.log("content-script");
 
-getToken().then(
-  (token: string): void => {
-    fetch("https://api.github.com/repos/YeThor/Pin-Github/issues", {
-      method: "POST",
-      headers: new Headers({
-        Authorization: `token ${token}`
-      }),
-      body: JSON.stringify({
-        title: "Test Title",
-        body: "Test Body"
-      })
-    }).then(res => console.log(res));
-  }
-);
+(function() {
+  const newIssueBtn = getNewIssueBtn();
 
-console.log("abasc");
+  if (!newIssueBtn) {
+    return;
+  }
+
+  console.log(newIssueBtn);
+  getToken().then(
+    (token: string): void => {
+      fetch("https://api.github.com/repos/YeThor/Pin-Github/issues", {
+        method: "POST",
+        headers: new Headers({
+          Authorization: `token ${token}`
+        }),
+        body: JSON.stringify({
+          title: "Test Title",
+          body: "Test Body"
+        })
+      }).then(res => console.log(res));
+    }
+  );
+})();
+
+function getNewIssueBtn(): HTMLElement | null {
+  return document.querySelector(
+    'a[href="/YeThor/Pin-Github/issues/new"][role="button"]'
+  );
+}
