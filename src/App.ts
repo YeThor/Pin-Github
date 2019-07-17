b02c9e13084accd5e4f75bfd016b477fbf471f0c
YeThor <kysloves@naver.com>
2019-07-18 01:00:14 +0900

    ♻️ refactor token related logic due to UI change

--- src/App.ts
+++ src/App.ts
@@ -1,87 +1,71 @@
 import { fromEvent, Observable } from "rxjs";
+import { filter } from "rxjs/operators";
 import getToken from "./util/getToken";
 
 export default class App {
-  private _onTokenBtnClick$: Observable<Event>;
+  private _tokenField!: HTMLDivElement;
+  private _tokenInput!: HTMLInputElement;
+  private _tokenLabel!: HTMLLabelElement;
 
-  constructor() {
-    this._onTokenBtnClick$ = fromEvent(
-      document.querySelector("#token-btn") as HTMLButtonElement,
-      "click"
-    );
+  public constructor() {
+    this._assignDOM();
+    this._attachEvents();
 
-    this._init();
-  }
-
-  private _init(): void {
-    this._watchObservables();
     getToken().then(
       (token: string): void => {
-        const tokenInput = document.querySelector(
-          "#token-input"
-        ) as HTMLInputElement;
-
-        if (token) {
-          tokenInput.value = token;
-          this._disableInput();
-        } else {
-          this._enableInput();
-        }
+        this._displayToken(token);
       }
     );
   }
 
-  private _watchObservables(): void {
-    this._onTokenBtnClick$.subscribe(() => {
-      this._onTokenBtnClick();
+  private _attachEvents(): void {
+    fromEvent(this._tokenInput, "blur").subscribe(() => {
+      this._tokenInput.type = "password";
+      this._tokenInput.disabled = true;
+      this._storeToken();
     });
+
+    fromEvent(this._tokenField, "click")
+      .pipe(filter(() => this._tokenInput.disabled === true))
+      .subscribe(() => {
+        this._tokenInput.type = "text";
+        this._tokenInput.disabled = false;
+        this._tokenInput.focus();
+      });
   }
 
-  private _onTokenBtnClick(): void {
-    const isEdit =
-      (document.querySelector("#token-btn") as HTMLButtonElement).innerText ===
-      "Edit";
+  private _assignDOM(): void {
+    this._tokenField = document.querySelector("#token") as HTMLDivElement;
 
-    isEdit ? this._enableInput() : this._storeToken();
+    this._tokenInput = document.querySelector(
+      "#token-input"
+    ) as HTMLInputElement;
+
+    this._tokenLabel = document.querySelector(
+      'label[for="token-input"]'
+    ) as HTMLLabelElement;
+  }
+
+  private _displayToken(token: string): void {
+    if (!token) {
+      return;
+    }
+
+    this._tokenInput.value = token;
+    this._tokenInput.type = "password";
+    this._tokenInput.classList.add("valid");
+    this._tokenInput.disabled = true;
+
+    this._tokenLabel.classList.add("active");
   }
 
   private _storeToken(): void {
-    const token = (document.querySelector("#token-input") as HTMLInputElement)
-      .value;
+    const token = this._tokenInput.value.trim();
 
-    chrome.storage.sync.set({ token }, () => {
-      this._disableInput();
-    });
-  }
+    if (token === "") {
+      return;
+    }
 
-  private _enableInput(): void {
-    const tokenInputBtn = document.querySelector(
-      "#token-input"
-    ) as HTMLInputElement;
-    const tokenSaveBtn = document.querySelector(
-      "#token-btn"
-    ) as HTMLButtonElement;
-
-    tokenInputBtn.type = "text";
-    tokenInputBtn.disabled = false;
-    tokenInputBtn.style.border = "2px solid grey";
-    tokenInputBtn.focus();
-
-    tokenSaveBtn.innerText = "Save";
-  }
-
-  private _disableInput(): void {
-    const tokenInputBtn = document.querySelector(
-      "#token-input"
-    ) as HTMLInputElement;
-    const tokenSaveBtn = document.querySelector(
-      "#token-btn"
-    ) as HTMLButtonElement;
-
-    tokenInputBtn.type = "password";
-    tokenInputBtn.disabled = true;
-    tokenInputBtn.style.border = "none";
-
-    tokenSaveBtn.innerText = "Edit";
+    chrome.storage.sync.set({ token });
   }
 }
