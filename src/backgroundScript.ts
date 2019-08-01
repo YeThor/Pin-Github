7c7fad9489ec838ffdb60a92de86a3718f21c3d6
YeThor <kysloves@naver.com>
2019-08-02 00:13:18 +0900

    :sparkles: run background-script on PR page routed with SPA

--- src/backgroundScript.ts
+++ src/backgroundScript.ts
@@ -1,8 +1,11 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
   const isIssueBoard = /https:\/\/github.com\/.*\/(issues)$/.test(details.url);
+  const isPREndPage = /https:\/\/github.com\/.*\/(pull)\/\d+$/.test(
+    details.url
+  );
 
-  if (isIssueBoard) {
-    console.log("IssueBoard!!");
+  if (isIssueBoard || isPREndPage) {
+    console.log("IssueBoard or PREndPage ", details.url);
     chrome.tabs.executeScript({
       file: "dist/contentScript.bundle.js"
     });
