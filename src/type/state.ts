104df0462530e0d75ff1f666e4404e69e5160173
YeThor <kysloves@naver.com>
2019-08-10 00:31:06 +0900

    :sparkles: add fields for pr

--- src/type/state.ts
+++ src/type/state.ts
@@ -1,8 +1,11 @@
 export default interface state {
-    [key: string]: string | string[];
-    token: string;
-    title: string;
-    assignees: string[];
-    labels: string[];
-    milestone: string;
-  }
+  [key: string]: string | string[];
+  token: string;
+  title: string;
+  assignees: string[];
+  prassignees: string[];
+  labels: string[];
+  prlabels: string[];
+  milestone: string;
+  prmilestone: string;
+}
