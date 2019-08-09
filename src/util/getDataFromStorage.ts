aac7ff8db3f2da7740f151445bf63d096dd480ff
YeThor <kysloves@naver.com>
2019-08-10 00:31:54 +0900

    :sparkles: enable editting PR with stored values

--- src/util/getDataFromStorage.ts
+++ src/util/getDataFromStorage.ts
@@ -3,7 +3,16 @@ import state from "../type/state";
 export default function getDataFromStorage(): Promise<state> {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get(
-      ["token", "title", "assignees", "labels", "milestone"],
+      [
+        "token",
+        "title",
+        "assignees",
+        "labels",
+        "milestone",
+        "prassignees",
+        "prlabels",
+        "prmilestone"
+      ],
       (result: any) => {
         resolve(result);
       }
